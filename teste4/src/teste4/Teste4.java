eac69e743c4de31312cee9595f6aaa10ccf324d0
gul-amd-moz <100712631+gul-amd-moz@users.noreply.github.com>
2022-03-29 15:24:50 +0200

    Introducao do Projecto
    projectio de estudo para a avaliacao 3 de programacao

---
+++ teste4/src/teste4/Teste4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package teste4;
+
+/**
+ *
+ * @author Gulamo_w10
+ */
+public class Teste4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
